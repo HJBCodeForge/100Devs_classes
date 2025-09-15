08dbcc22ab1c203e77d44bdb7cd72e584723a4ae
Henning <hjb.codeforge@gmail.com>
2025-09-15 09:16:32 -0400

    Server code optimization

--- class35-materials/node-backend-simple-json/server.js
+++ class35-materials/node-backend-simple-json/server.js
@@ -8,55 +8,55 @@ const server = http.createServer((req, res) => {
  
   const readWrite = (file, contentType) => {
     fs.readFile(file, function(err, data) {
-    res.writeHead(200, {'Content-Type': contentType});
-    res.write(data);
-    res.end();
+      res.writeHead(200, {'Content-Type': contentType});
+      res.write(data);
+      res.end();
     });
   }
 
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
-  if (page == '/') {
-    readWrite('index.html', 'text/html')
-  }
-  else if (page == '/otherpage') {
-    readWrite('otherpage.html', 'text/html')
-  }
-  else if (page == '/otherotherpage') {
-    readWrite('otherotherpage.html', 'text/html')
-  }
-  else if (page == '/api') {
-    if('student' in params){
+
+  switch (page) {
+    case '/':
+      readWrite('index.html', 'text/html')
+    break
+    case '/otherpage':
+      eadWrite('otherpage.html', 'text/html')
+    break
+    case '/otherotherpage':
+      readWrite('otherotherpage.html', 'text/html')
+    break
+    case '/api':
+      let personName = "unknown"
+      let personStatus = "unknown"
+      let personCurrentOccupation = "unknown"
+
       if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "leon",
-          status: "Boss Man",
-          currentOccupation: "Baller"
-        }
-        res.end(JSON.stringify(objToJson));
+        personName = "leon",
+        personStatus = "Boss Man",
+        personCurrentOccupation = "Baller"
       }//student = leon
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
+      res.writeHead(200, {'Content-Type': 'application/json'});
         const objToJson = {
-          name: "unknown",
-          status: "unknown",
-          currentOccupation: "unknown"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
+          name: personName,
+          status: personStatus,
+          currentOccupation: personCurrentOccupation
+      }
+      res.end(JSON.stringify(objToJson));
+    break
+    case '/css/style.css':
+      fs.readFile('css/style.css', function(err, data) {
       res.write(data);
       res.end();
-    });
-  }else if (page == '/js/main.js'){
-    readWrite('js/main.js', 'text/javascript')
-  }else{
-    figlet('404!!', function(err, data) {
+      });
+    break
+    case '/js/main.js':
+      readWrite('js/main.js', 'text/javascript')
+    break
+    default:
+      figlet('404!!', function(err, data) {
       if (err) {
           console.log('Something went wrong...');
           console.dir(err);
